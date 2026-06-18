ed2aedc5be382bcf26d614b765ddb1354477e2bb
pjmoo <_00pjmoooo@gmail.com>
2026-06-18 14:30:45 +0900

    feat: 자바 Set(HashSet) 실습 예제 추가

---
+++ src/Ex08.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ex08 {
+    public static void main(String[] args){
+        Set<String> set = new HashSet<>();
+        set.add("원이");
+        set.add("미나미");
+        set.add("제나");
+        set.add("리브");
+        set.add("메이");
+    }
+}
